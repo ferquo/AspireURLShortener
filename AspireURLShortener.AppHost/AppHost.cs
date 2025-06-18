af8229cbfba2c0f022d65987f35aa0f4b863a337
ferquo <ferquo07@gmail.com>
2025-06-18 14:51:22 +0300

    Added postgres db

--- AspireURLShortener.AppHost/AppHost.cs
+++ AspireURLShortener.AppHost/AppHost.cs
@@ -1,6 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.AspireURLShortener_ApiService>("apiservice")
+var sqlPassword = builder.AddParameter("PostgresSqlPassword", secret: true);
+var sqlUsername = builder.AddParameter("PostgresSqlUsername", secret: true);
+
+var postgres = builder.AddPostgres("postgres",sqlUsername, sqlPassword).WithDataVolume();
+var postgresDb = postgres.AddDatabase("postgresDb");
+
+var apiService = builder
+    .AddProject<Projects.AspireURLShortener_ApiService>("apiService")
+    .WithReference(postgresDb)
+    .WaitFor(postgresDb)
     .WithHttpHealthCheck("/health");
 
 builder.Build().Run();
